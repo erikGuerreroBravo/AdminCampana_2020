8f6c5807bab3351632803f440b886ff5eb86fab9
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2020-01-26 16:15:03 -0600

    Agregar archivos de proyecto.

---
+++ AdminCampana_2020/Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace AdminCampana_2020.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [HttpGet]
+        public ActionResult InternalServerError()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+    }
+}

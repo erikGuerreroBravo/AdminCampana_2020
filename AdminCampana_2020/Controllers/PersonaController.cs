d52b6810cf31b46b2bd12a5c05f6067113222f3b
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2020-02-12 15:19:46 -0600

    se instalo el plugin de toaster.js

--- AdminCampana_2020/Controllers/PersonaController.cs
+++ AdminCampana_2020/Controllers/PersonaController.cs
@@ -40,11 +40,28 @@ namespace AdminCampana_2020.Controllers
 
         [HttpPost]
         [AllowAnonymous]
-        public ActionResult Registro([Bind(Include ="")]PersonaVM personaVM)
-        {
+        public ActionResult Registro([Bind(Include = "StrNombre,StrApellidoPaterno,StrApellidoMaterno,StrEmail,TelefonoVM,StrNumeroCelular,DireccionVM")]PersonaVM personaVM,string IdColonia,string IdSeccion,string IdZona,string IdEstrategias)
+        {  
+            var coloniaId = int.Parse(IdColonia);
+            var seccionId = int.Parse(IdSeccion);
+            var zonaId = int.Parse(IdZona);
+            var estrategiaId = int.Parse(IdEstrategias);
+
+            personaVM.EstrategiaVM = new EstrategiaVM();
+            personaVM.EstrategiaVM.Id = estrategiaId;
+
+            personaVM.DireccionVM.SeccionVM = new SeccionVM();
+            personaVM.DireccionVM.SeccionVM.Id = seccionId;
+
+            personaVM.DireccionVM.ColoniaVM = new ColoniaVM();
+            personaVM.DireccionVM.ColoniaVM.Id = coloniaId;
+
+            personaVM.DireccionVM.ZonaVM = new ZonaVM();
+            personaVM.DireccionVM.ZonaVM.Id = zonaId;
+
             if (ModelState.IsValid)
             {
-
+                
             }
 
             ViewBag.IdColonia = new SelectList(IcoloniaBusiness.GetColonias(), "id", "strAsentamiento");
---
+++ AdminCampana_2020/Scripts/persona/message-confirmation.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+
+    $('#btnEnviar').click(function ()
+    {
+        toastr.success("Datos Guardados Correctamente.", "App-Campaña dice", { timeOut: 1000, closeButton: true });
+    });
+
+   
+
+});

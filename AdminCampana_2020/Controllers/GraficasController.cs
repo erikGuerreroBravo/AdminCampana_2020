316731c0833b0ace8ac6d5b87abd0315119a2d99
FazurP <limon-amargoverde15@hotmail.com>
2020-02-19 15:51:54 -0600

    Graficas Estaticas.

---
+++ AdminCampana_2020/Controllers/GraficasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdminCampana_2020.Controllers
+{
+    public class GraficasController : Controller
+    {
+        // GET: Graficas
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}

238df9e0b8204a14c8417d2d3fdf25885fed559a
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2020-02-20 16:04:24 -0600

    Se aregaron las clases e interfaces  del esquema de seguridad en la capa de business.

---
+++ AdminCampana_2020.Business/Interface/IUsuarioBusiness.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminCampana_2020.Business.Interface
+{
+    public interface IUsuarioBusiness
+    {
+
+    }
+}
---
+++ AdminCampana_2020.Business/UsuarioBusiness.cs
@@ -0,0 +1,13 @@
+﻿using AdminCampana_2020.Business.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminCampana_2020.Business
+{
+    public class UsuarioBusiness:IUsuarioBusiness
+    {
+    }
+}

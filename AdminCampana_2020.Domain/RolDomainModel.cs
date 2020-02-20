daf85d3d4dcbcd95814d80b2cc5e088da6bacb9b
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2020-02-20 15:57:20 -0600

    Se agregaron las calses de domain model correspondientes al modelo de seguridad.

---
+++ AdminCampana_2020.Domain/RolDomainModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminCampana_2020.Domain
+{
+    public class RolDomainModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public List<UsuarioDomainModel> UsuarioRolesDM { get; set; }
+    }
+}
---
+++ AdminCampana_2020.Domain/UsuarioRolDomainModel.cs
@@ -0,0 +1,14 @@
+﻿using AdminCampana_2020.Repository;
+
+namespace AdminCampana_2020.Domain
+{
+    public class UsuarioRolDomainModel
+    {
+        public int Id { get; set; }
+        public int IdUsuario { get; set; }
+        public int IdRol { get; set; }
+
+        public UsuarioDomainModel Usuario { get; set; }
+        public RolDomainModel Rol { get; set; }
+    }
+}

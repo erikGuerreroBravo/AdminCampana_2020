fd3e16017c9957096539b561e00ecfa05cce552e
Gabriela Soledad <gabysm15@outlook.com>
2020-02-20 18:56:39 -0600

    Generación de scripts

--- AdminCampana_2020/Scripts/persona/message-confirmation.js
+++ AdminCampana_2020/Scripts/persona/message-confirmation.js
@@ -1,11 +1,19 @@
 ﻿$(document).ready(function () {
 
-    $('#btnEnviar').click(function ()
+    $('#btnEnviar').click(function (e)
     {
-        toastr.success("Datos Guardados Correctamente.", "App-2020 dice", { timeOut: 1000, closeButton: true });
+        var correoI = $('#Correo').val();
+
+        var regex = new RegExp("^[a-zA-Z0-9._-]{3,25}[@]{1}(hotmail|gmail|outlook|yahoo){1}[.]{1}(com|mx|net|es){1}$");
+        debugger
+        if (!regex.test(correoI)) {
+            e.preventDefault();
+            toastr.warning('Ingresa un valor valido', 'Campaña 2020 dice', { timeOut: 1000, closeButton: true });
+            return false;
+        } else {
+            toastr.success("Datos Guardados Correctamente.", "App-2020 dice", { timeOut: 1000, closeButton: true });
         //window.location.replace("http://localhost:51085/Home/Index");
+            return true;
+        }       
     });
-
-   
-
 });

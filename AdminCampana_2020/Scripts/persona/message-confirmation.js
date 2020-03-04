49673b05abbf47a9882f0f9764b8b8120753646a
Gabriela Soledad <gabysm15@outlook.com>
2020-03-04 11:20:39 -0600

    Deshabilitación de validación JS de correo electrónico.

--- AdminCampana_2020/Scripts/persona/message-confirmation.js
+++ AdminCampana_2020/Scripts/persona/message-confirmation.js
@@ -2,18 +2,24 @@
       
     $('#btnEnviar').click(function (e)
     {
-        var correoI = $('#Correo').val();
-
-        var regex = new RegExp("^[a-zA-Z0-9._-]{3,25}[@]{1}(hotmail|gmail|outlook|yahoo){1}[.]{1}(com|mx|net|es){1}$");
-        debugger
-        if (!regex.test(correoI)) {
-            e.preventDefault();
-            toastr.warning('Ingresa un valor valido', 'Campaña 2020 dice', { timeOut: 1000, closeButton: true });
-            return false;
-        } else {
+        
             toastr.success("Datos Guardados Correctamente.", "App-2020 dice", { timeOut: 1000, closeButton: true });
-        //window.location.replace("http://localhost:51085/Home/Index");
-            return true;
-        }       
+            //window.location.replace("http://localhost:51085/Home/Index");
+            return true;         
+
+        //Validación de correo electrónico
+        //var correoI = $('#Correo').val();
+
+        //var regex = new RegExp("^[a-zA-Z0-9._-]{3,25}[@]{1}(hotmail|gmail|outlook|yahoo){1}[.]{1}(com|mx|net|es){1}$");
+        
+        //if (!regex.test(correoI)) {
+        //    e.preventDefault();
+        //    toastr.warning('Ingresa un valor valido', 'Campaña 2020 dice', { timeOut: 1000, closeButton: true });
+        //    return false;
+        //} else {
+        //    toastr.success("Datos Guardados Correctamente.", "App-2020 dice", { timeOut: 1000, closeButton: true });
+        ////window.location.replace("http://localhost:51085/Home/Index");
+        //    return true;
+        //}       
     });
 });

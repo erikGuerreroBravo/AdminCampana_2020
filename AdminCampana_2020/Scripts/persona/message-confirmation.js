1dd3bbb111253f6d21044e866d6a21c3d9aa5649
Gabriela Soledad <gabysm15@outlook.com>
2020-03-04 11:21:26 -0600

    Deshabilitación de validación de correo electrónico de JS.

--- AdminCampana_2020/Scripts/persona/message-confirmation.js
+++ AdminCampana_2020/Scripts/persona/message-confirmation.js
@@ -8,6 +8,8 @@
             return true;         
 
         //Validación de correo electrónico
+
+
         //var correoI = $('#Correo').val();
 
         //var regex = new RegExp("^[a-zA-Z0-9._-]{3,25}[@]{1}(hotmail|gmail|outlook|yahoo){1}[.]{1}(com|mx|net|es){1}$");
